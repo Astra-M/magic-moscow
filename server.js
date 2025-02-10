a591246b9e3d8dffb86547c8efb92f1138064f5e
Astra <asparagus_m@mail.ru>
2025-02-10 15:43:02 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+const { PORT = 3000, BASE_PATH } = process.env;
+const path = require("path");
+const app = express();
+app.use(helmet());
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server location is on ${BASE_PATH}`);
+  console.log(`Serving static files from ${path.join(__dirname, "public")}`);
+});
