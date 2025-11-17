1bb0a453c41ed4cd80c7b88a0d5068d2dbff3b65
Astra <asparagus_m@mail.ru>
2025-11-17 18:42:43 +0300

    бэкап перед установкой tailwind

--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,9 @@
 const menu = document.querySelector(".menu");
 const burger = document.querySelector(".burger");
-// const burger = document.querySelector(".burger__icon");
-const header = document.querySelector(".header");
 const page = document.querySelector(".page");
 
 burger.addEventListener("click", (e) => {
   burger.classList.toggle("burger_opened");
-  // burger.classList.toggle("burger__icon_active");
   menu.classList.toggle("menu_opened");
-  // menu.classList.toggle("header__navigation_mobile");
-  // header.classList.toggle("header_100vh");
   page.classList.toggle("page_locked");
-  // console.log("скрипт выполнен");
 });
-// const menu = document.querySelector(".menu");
-
-// const body = document.body;
-// if (menu & burger) {
-//   menu.classList.toggle(".menu_visible");
-//   burger.classList.add(".burger_hidden");
-// }
-// if (menu && menuBtn) {
-// 	menuBtn.addEventListener('click', e => {
-// 		menu.classList.toggle('active')
-// 		menuBtn.classList.toggle('active')
-// 		body.classList.toggle('lock')
-// 	})
-
-// 	menu.addEventListener('click', e => {
-// 		if (e.target.classList.contains('menu__body')) {
-// 			menu.classList.remove('active')
-// 			menuBtn.classList.remove('active')
-// 			body.classList.remove('lock')
-// 		}
-// 	})
-
-// 	menu.querySelectorAll('.menu__link').forEach(link => {
-// 		link.addEventListener('click', () => {
-// 			menu.classList.remove('active')
-// 			menuBtn.classList.remove('active')
-// 			body.classList.remove('lock')
-// 		})
-// 	})
-// }
