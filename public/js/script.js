2a823ec1f5cac8bbc11dfcf7b07fd47cf224ff21
Astra <asparagus_m@mail.ru>
2025-11-29 14:47:51 +0300

    исправить появление лого на мобильном меню

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector(".menu");
 const burger = document.querySelector(".burger");
-const page = document.querySelector(".page");
+const page = document.body;
+const logo = document.querySelector(".logo");
 
 burger.addEventListener("click", (e) => {
   burger.classList.toggle("burger_opened");
   menu.classList.toggle("menu_opened");
   page.classList.toggle("page_locked");
+  logo.classList.toggle("logo_hidden");
 });
