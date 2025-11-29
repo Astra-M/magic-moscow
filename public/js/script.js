10b8f7d49b764286af7205ef36f62c41ef706712
Astra <asparagus_m@mail.ru>
2025-11-29 16:12:44 +0300

    разделить меню десктопа и мобильных

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
-const menu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 const burger = document.querySelector(".burger");
 const page = document.body;
 const logo = document.querySelector(".logo");
 
 burger.addEventListener("click", (e) => {
   burger.classList.toggle("burger_opened");
-  menu.classList.toggle("menu_opened");
+  mobileMenu.classList.toggle("mobile-menu_opened");
   page.classList.toggle("page_locked");
   logo.classList.toggle("logo_hidden");
 });
