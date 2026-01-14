89acdcf87e9d2efa2a38207aeb7515187fac5953
Astra <asparagus_m@mail.ru>
2026-01-14 21:34:25 +0300

    добавить скролл вертикальный в меню

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,15 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const burger = document.querySelector(".burger");
 const page = document.body;
-const logo = document.querySelector(".logo");
+// const logo = document.querySelector(".logo");
 const toggleButtons = document.querySelectorAll(".mobile-menu__toggle");
-const excursionsItem = document.querySelector(".mobile-menu__item--excursions");
-const excursionsSubmenu = document.querySelector(".mobile-menu__submenu");
-const subitems = document.querySelectorAll(".mobile-menu__subitem");
+
 burger.addEventListener("click", (e) => {
   burger.classList.toggle("burger_opened");
   mobileMenu.classList.toggle("mobile-menu_opened");
   page.classList.toggle("page_locked");
-  logo.classList.toggle("logo_hidden");
+  // logo.classList.toggle("logo_hidden");
 });
-// excursionsItem.addEventListener("click", (e) => {
-//   e.stopPropagation();
-//   excursionsSubmenu.classList.toggle("mobile-menu__submenu--opened");
-//   console.log("mobile-menu__submenu--opened");
-// });
-
-// subitems.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     item.classList.add("mobile-menu__subitem--active");
-//   });
-// });
-
-
 
 toggleButtons.forEach((button) => {
   const submenu = button
